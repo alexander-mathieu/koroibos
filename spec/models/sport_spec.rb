45ae87e0b56268524f8690abc679658066cee165
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-14 16:13:34 -0600

    Add additional validation to Sport

--- spec/models/sport_spec.rb
+++ spec/models/sport_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Sport, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
   end
+  
   describe 'relationships' do
     it { should have_many :olympian_sports }
     it { should have_many(:olympians).through(:olympian_sports) }
