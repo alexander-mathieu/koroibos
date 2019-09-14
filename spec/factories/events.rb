b1b2819324bc017d6c664afceee23d332cd8bd31
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-14 15:23:52 -0600

    Add Event model with initial spec, factory and relationships

---
+++ spec/factories/events.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :event do
+    sport
+    name { "MyString" }
+  end
+end
---
+++ spec/models/event_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Event, type: :model do
+  describe 'validates' do
+    it { should validate_presence_of :name }
+  end
+
+  describe 'relationships' do
+    it { should have_many :olympian_events }
+    it { should have_many(:olympians).through(:olympian_events) }
+  end
+end
