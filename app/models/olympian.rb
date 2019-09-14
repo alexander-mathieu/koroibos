b953b9e891503ac881e7c11ef4294d5364a301ab
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-14 16:13:14 -0600

    Add additional relationships to Olympian

--- app/models/olympian.rb
+++ app/models/olympian.rb
@@ -2,8 +2,8 @@ class Olympian < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   belongs_to :team
-
-  has_many :events
+  has_many :olympian_events
+  has_many :events, through: :olympian_events
   has_many :olympian_sports
   has_many :sports, through: :olympian_sports
 end
