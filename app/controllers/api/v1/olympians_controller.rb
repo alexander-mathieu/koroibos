3db1efe2719b3ebaf14f67eac6f8eef9b0d5ebda
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-14 23:25:46 -0600

    Add conditional logic for age filtering to OlympiansController

--- app/controllers/api/v1/olympians_controller.rb
+++ app/controllers/api/v1/olympians_controller.rb
@@ -1,11 +1,19 @@
 class Api::V1::OlympiansController < ApplicationController
   def index
-    render json: OlympianSerializer.new(
-      Olympian.includes(
+    if params[:age]
+      if params[:age] == 'youngest'
+        olympian_data = Olympian.youngest_olympian
+      else
+        olympian_data = Olympian.oldest_olympian
+      end
+    else
+      olympian_data = Olympian.includes(
         :team,
         :events,
         :sports
-        )
       )
+    end
+
+    render json: OlympianSerializer.new(olympian_data)
   end
 end
