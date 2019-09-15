391c530ef5575ff42b6a482a1655961c19baf7e2
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-14 23:28:29 -0600

    Refactor Olympian model spec

--- spec/models/olympian_spec.rb
+++ spec/models/olympian_spec.rb
@@ -1,6 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Olympian, type: :model do
+  before :each do
+    @o1, @o2, @o3, @o4 = create_list(:olympian, 4)
+    @o5 = create(:olympian, age: 14)
+    @o6 = create(:olympian, age: 42)
+
+    create_list(:olympian_event_with_medal, 4, olympian: @o2)
+    create_list(:olympian_event_with_medal, 2, olympian: @o4)
+    create_list(:olympian_event_with_medal, 1, olympian: @o1)
+    create_list(:olympian_event_without_medal, 2, olympian: @o4)
+    create_list(:olympian_event_without_medal, 1, olympian: @o2)
+    create_list(:olympian_event_without_medal, 0, olympian: @o3)
+  end
+
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_uniqueness_of :name }
@@ -16,37 +29,20 @@ RSpec.describe Olympian, type: :model do
 
   describe 'instance methods' do
     it '#total_medal_count' do
-      o1, o2, o3, o4 = create_list(:olympian, 4)
-
-      create_list(:olympian_event_with_medal, 4, olympian: o2)
-      create_list(:olympian_event_with_medal, 2, olympian: o4)
-      create_list(:olympian_event_with_medal, 1, olympian: o1)
-      create_list(:olympian_event_without_medal, 2, olympian: o4)
-      create_list(:olympian_event_without_medal, 1, olympian: o2)
-      create_list(:olympian_event_without_medal, 0, olympian: o3)
-
-      expect(o1.total_medal_count).to eq(1)
-      expect(o2.total_medal_count).to eq(4)
-      expect(o3.total_medal_count).to eq(0)
-      expect(o4.total_medal_count).to eq(2)
+      expect(@o1.total_medal_count).to eq(1)
+      expect(@o2.total_medal_count).to eq(4)
+      expect(@o3.total_medal_count).to eq(0)
+      expect(@o4.total_medal_count).to eq(2)
     end
   end
 
   describe 'class methods' do
     it '.youngest_olympian' do
-      create_list(:olympian, 4)
-
-      youngest_olympian = create(:olympian, age: 14)
-
-      expect(Olympian.youngest_olympian).to eq(youngest_olympian)
+      expect(Olympian.youngest_olympian).to eq(@o5)
     end
 
     it '.oldest_olympian' do
-      create_list(:olympian, 4)
-      
-      oldest_olympian = create(:olympian, age: 42)
-
-      expect(Olympian.oldest_olympian).to eq(oldest_olympian)
+      expect(Olympian.oldest_olympian).to eq(@o6)
     end
   end
 end
