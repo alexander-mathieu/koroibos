d419a6fdea0807e258f0ec8d301e0cea492802fc
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-15 17:21:49 -0600

    Add MedalistsController#index and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/events', to: 'events#index'
+      get '/medalists', to: 'medalists#index'
       get '/olympians', to: 'olympians#index'
       get '/olympian_stats', to: 'olympian_stats#index'
     end
