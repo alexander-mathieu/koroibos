ff0e9757087228ae621c2cbe57c796faad2d7a76
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-14 23:14:19 -0600

    Add spec for olympians endpoint youngest olympian query

--- spec/requests/api/v1/olympians_endpoint_spec.rb
+++ spec/requests/api/v1/olympians_endpoint_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Olympians endpoint' do
+  before :each do
+    @o1, @o2, @o3, @o4 = create_list(:olympian, 4)
+    @o5 = create(:olympian, age: 14)
+    @o6 = create(:olympian, age: 42)
+
+    create_list(:olympian_event, 3, olympian: @o1)
+    create_list(:olympian_event, 3, olympian: @o2)
+    create_list(:olympian_event, 3, olympian: @o3)
+    create_list(:olympian_event, 3, olympian: @o4)
+  end
+
   it 'returns a list of all Olympians' do
-    o1, o2, o3, o4 = create_list(:olympian, 4)
-
-    create_list(:olympian_event, 3, olympian: o1)
-    create_list(:olympian_event, 3, olympian: o2)
-    create_list(:olympian_event, 3, olympian: o3)
-    create_list(:olympian_event, 3, olympian: o4)
-
     get '/api/v1/olympians'
 
     expect(response).to be_successful
@@ -16,7 +20,7 @@ RSpec.describe 'Olympians endpoint' do
     olympians = JSON.parse(response.body, symbolize_names: true)[:data]
     first_olympian = olympians[0][:attributes]
 
-    expect(olympians.count).to eq(4)
+    expect(olympians.count).to eq(6)
 
     expect(first_olympian).to have_key(:age)
     expect(first_olympian[:age]).to be_an(Integer)
@@ -32,5 +36,15 @@ RSpec.describe 'Olympians endpoint' do
 
     expect(first_olympian).to have_key(:total_medal_count)
     expect(first_olympian[:total_medal_count]).to be_an(Integer)
+  end
+
+  it 'returns the youngest Olympian' do
+    get '/api/v1/olympians?age=youngest'
+
+    expect(response).to be_successful
+
+    olympian = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+
+    expect(olympian[:age]).to eq(14)
   end
 end
