4cec80f57940b469c879289e15390b704ca7f10b
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-15 19:13:40 -0600

    Add Team#total_medal_count and #medal_count with specs

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,18 @@ class Team < ApplicationRecord
   validates :name, presence: true
 
   has_many :olympians
+
+  def total_medal_count
+    olympians
+    .joins(:events)
+    .merge(OlympianEvent.medalled_events)
+    .count
+  end
+
+  def medal_count(medal)
+    olympians
+    .joins(:events)
+    .where(olympian_events: { medal: medal })
+    .count
+  end
 end
