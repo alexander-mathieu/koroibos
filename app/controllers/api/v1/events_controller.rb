0a5c65c33aeceeecd5fba4650dc1b1be92f5f2fe
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-16 12:27:22 -0600

    Rename SportSerializer to SportEventSerializer for clarity

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::EventsController < ApplicationController
   def index
-    render json: SportSerializer.new(Sport.includes(:events))
+    sports = Sport.includes(:events)
+    render json: SportEventSerializer.new(sports)
   end
 end
