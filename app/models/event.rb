2982ca28bccf51257b125805eabe664a9e6b9c52
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-15 20:58:49 -0600

    Add Event.team_events and .team_participation_percentage with specs

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,16 @@ class Event < ApplicationRecord
   belongs_to :sport
   has_many :olympian_events
   has_many :olympians, through: :olympian_events
+
+  def self.team_participation_percentage(team_id)
+    (team_events(team_id).count).fdiv(self.count)
+  end
+
+  def self.team_events(team_id)
+    select('events.*')
+    .joins(olympians: :team)
+    .where(teams: { id: team_id })
+    .uniq
+    .pluck(:name)
+  end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -11,4 +11,38 @@ RSpec.describe Event, type: :model do
     it { should have_many :olympian_events }
     it { should have_many(:olympians).through(:olympian_events) }
   end
+
+  describe 'class methods' do
+    before :each do
+      @t1 = create(:team)
+      @t2 = create(:team)
+
+      @o1 = create(:olympian, team: @t1)
+      @o2 = create(:olympian, team: @t1)
+      @o3 = create(:olympian, team: @t2)
+      @o4 = create(:olympian, team: @t2)
+
+      @e1 = create(:event, name: 'Event 1')
+      @e2 = create(:event, name: 'Event 2')
+      @e3 = create(:event, name: 'Event 3')
+      @e4 = create(:event, name: 'Event 4')
+
+      create(:olympian_event_with_medal, event: @e1, olympian: @o1)
+      create(:olympian_event_with_medal, event: @e1, olympian: @o2)
+      create(:olympian_event_with_medal, event: @e2, olympian: @o1)
+      create(:olympian_event_with_medal, event: @e2, olympian: @o2)
+      create(:olympian_event_with_medal, event: @e3, olympian: @o3)
+      create(:olympian_event_with_medal, event: @e4, olympian: @o1)
+    end
+
+    it '.team_participation_percentage' do
+      expect(Event.team_participation_percentage(@t1.id)).to eq(0.75)
+      expect(Event.team_participation_percentage(@t2.id)).to eq(0.25)
+    end
+
+    it '.team_events' do
+      expect(Event.team_events(@t1.id)).to eq(['Event 1', 'Event 2', 'Event 4'])
+      expect(Event.team_events(@t2.id)).to eq(['Event 3'])
+    end
+  end
 end
